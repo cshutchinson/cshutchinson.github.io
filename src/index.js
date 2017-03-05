e6974f8e2760a5662129aca44eda314aa20d5d55
Christopher Hutchinson <cshutchinson@gmail.com>
2017-03-05 09:50:59 -0700

    added source for those looking

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from '../modules/App';
+import { Router, Route, IndexRoute, useRouterHistory } from 'react-router';
+import { createHistory } from 'history';
+
+import About from '../modules/About';
+import Repos from '../modules/Repos';
+
+const history = useRouterHistory(createHistory)({
+    baseName: '/sandbox/dgc-tooling-rita/'
+});
+
+render((
+  <Router history={history}>
+    <Route path="/" component={App}>
+        <Route path="/repos" component={Repos}/>
+        <Route path="/about" component={About}/>
+    </Route>
+  </Router>
+), document.getElementById('app'));
+
+
+
